b789783403e9724e10fda308b1823ca278a9ec85
OPApple <80098382+OPApple@users.noreply.github.com>
2022-08-15 19:11:14 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿while(true){
+    Console.Write("Enter a binary number: ");
+    string num =  Console.ReadLine();
+
+    for (int i = 2; i < 10; i++){
+        bool notBinary = num.Contains(i.ToString());
+        if (notBinary){
+            Console.WriteLine("Bruh");
+            break;
+        }else{
+
+           
+        }
+        
+    }
+    converter(num);
+
+
+}
+
+static void converter(string num){
+    long result = 0;
+    for(int i = 0; i < num.Length; i++){
+        if(num[num.Length - 1 - i] == '1'){
+            result += Convert.ToInt64(Math.Pow(2, i));
+        }
+
+    }
+
+    Console.WriteLine("In base 10 that is: " + result);
+    
+    
+}
